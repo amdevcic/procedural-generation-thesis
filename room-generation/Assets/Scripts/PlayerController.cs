7db7d12086584b992819d4a3bb891f6aedc65efc
Ana Marija <devcicanamarija@gmail.com>
2022-05-12 22:52:44 +0200

    Fixed mouse look

--- room-generation/Assets/Scripts/PlayerController.cs
+++ room-generation/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     Camera playerCamera;
     public float speed = 10f;
     public float mouseSensitivity = 10f;
+    [Range(0, 90)]
+    public float turnLimit;
+    private float xrotation = 0, yrotation = 0;
 
     void Start()
     {
@@ -31,13 +34,16 @@ public class PlayerController : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        xrotation = Mathf.Clamp(xrotation-Input.GetAxis("Mouse Y")*mouseSensitivity, -turnLimit, turnLimit);
+        yrotation += Input.GetAxis("Mouse X")*mouseSensitivity;
         Vector3 move = transform.right * x + transform.forward * z;
         controller.SimpleMove(move*speed);
 
-        transform.Rotate(0, Input.GetAxis("Mouse X")*mouseSensitivity, 0);
-        playerCamera.transform.Rotate(
-            -Input.GetAxis("Mouse Y")*mouseSensitivity, 
-            0, 0);
+        // transform.Rotate(0, Input.GetAxis("Mouse X")*mouseSensitivity, 0);
+        // playerCamera.transform.Rotate(
+        //     -Input.GetAxis("Mouse Y")*mouseSensitivity, 
+        //     0, 0);
+        transform.rotation = Quaternion.Euler(xrotation, yrotation, 0);
 
     }
 }
